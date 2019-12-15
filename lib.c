10d13c2becbd27ca4993313b3fcc387bc23278c5
ggrubich <g.grubich.898@studms.ug.edu.pl>
2019-12-16 00:39:26 +0100

    Add a sorting function

---
+++ lib.c
@@ -0,0 +1,26 @@
+#include "lib.h"
+
+static void swap(int *a, int *b)
+{
+	int c = *a;
+	*a = *b;
+	*b = c;
+}
+
+// sort function uses the selection sort algorithm
+void sort(int *xs, int n)
+{
+	int i, j;
+	int min;
+	for (i = 0; i < n; ++i) {
+		min = i;
+		for (j = i + 1; j < n; ++j) {
+			if (xs[j] < xs[i]) {
+				min = j;
+			}
+		}
+		if (min != i) {
+			swap(&xs[i], &xs[min]);
+		}
+	}
+}
